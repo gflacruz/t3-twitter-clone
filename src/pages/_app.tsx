65ba29aaeb84d6103dc08b5b6770755e7f67a08e
gflacruz <glacruz@zoho.com>
2023-05-09 20:21:48 -0400

    removed t3 boiler plate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { SideNav } from "~/components/SideNav";
+
+import Head from 'next/head'
 
 import { api } from "~/utils/api";
 
@@ -12,7 +15,18 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Twitter Clone</title>
+        <meta name="description" content="this is a Twitter clone by Gabriel La Cruz"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
