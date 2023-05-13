1c8a9ffed57d76cb14ffdb88e42a174658050093
gflacruz <glacruz@zoho.com>
2023-05-12 22:09:44 -0400

    changed Profile images prop due to build error

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-// import { VscAccount } from "react-icons/vsc";
+import { VscAccount } from "react-icons/vsc";
 
 type ProfileImageProps = {
   src?: string | null;
@@ -11,11 +11,11 @@ export function ProfileImage({ src, className = "" }: ProfileImageProps) {
     <div
       className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}
     >
-      {/* {src == null ? (
+      {src == null ? (
         <VscAccount className="h-full w-full" />
-      ) : ( */}
+      ) : (
         <Image src={src} alt="Profile Image" quality={100} fill />
-      {/* )} */}
+      )}
     </div>
   );
 }
