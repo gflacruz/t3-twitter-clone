72d6fe85c59b3e91234020b32c7607a3fec985b6
gflacruz <glacruz@zoho.com>
2023-05-12 22:07:00 -0400

    added changes

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,3 +1,42 @@
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { IconHoverEffect } from './IconHoverEffect'
+import { VscHome } from 'react-icons/vsc'
+
 export function SideNav() {
-    return <h1>Nav</h1>
+    const session = useSession()
+    const user = session.data?.user
+
+    return (
+        <nav className="sticky top-0 px-2 py-4">
+            <ul className='flex flex-col items-start gap-2 whitespace-nowrap'>
+                <li>
+                    <Link href="/">
+                      <IconHoverEffect>
+                        <span className='flex items-center gap-4'>
+                          <VscHome className='h-8 w-8' />
+                          <span className='hidden text-lg md:inline'>
+                            Home
+                          </span>
+                        </span>
+                      </IconHoverEffect>
+                    </Link>
+                </li>
+                {user != null && (
+                    <li>
+                        <Link href={`/profiles/${user.id}`}>Profile</Link>
+                    </li>
+                )}
+                {user == null ? (
+                  <li>
+                    <button onClick={() => void signIn()}>Log In</button>
+                  </li>  
+                ) : (
+                  <li>
+                    <button onClick={() => void signOut()}>Log Out</button>
+                  </li>
+                )}
+            </ul>
+        </nav>
+    )
 }
